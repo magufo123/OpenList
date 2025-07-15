f14ebe74b286ae11acd7d841a36c4aef6570d22a
lyy2005a2 <long2005a2@outlook.com>
2025-07-15 05:57:03 +0000

    feat（115_open）：添加离线下载

--- internal/offline_download/tool/download.go
+++ internal/offline_download/tool/download.go
@@ -6,11 +6,13 @@ import (
 
 	"github.com/OpenListTeam/OpenList/v4/internal/conf"
 	"github.com/OpenListTeam/OpenList/v4/internal/errs"
+	"github.com/OpenListTeam/OpenList/v4/internal/model"
+	"github.com/OpenListTeam/OpenList/v4/internal/op"
 	"github.com/OpenListTeam/OpenList/v4/internal/setting"
 	"github.com/OpenListTeam/OpenList/v4/internal/task"
+	"github.com/OpenListTeam/tache"
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
-	"github.com/OpenListTeam/tache"
 )
 
 type DownloadTask struct {
@@ -101,6 +103,9 @@ outer:
 		}
 		return nil
 	}
+	if t.tool.Name() == "115 Open" {
+		return nil
+	}
 	t.Status = "offline download completed, maybe transferring"
 	// hack for qBittorrent
 	if t.tool.Name() == "qBittorrent" {
@@ -164,11 +169,32 @@ func (t *DownloadTask) Update() (bool, error) {
 
 func (t *DownloadTask) Transfer() error {
 	toolName := t.tool.Name()
-	if toolName == "115 Cloud" || toolName == "PikPak" || toolName == "Thunder" || toolName == "ThunderBrowser" {
+	if toolName == "115 Cloud" || toolName == "115 Open" || toolName == "PikPak" || toolName == "Thunder" || toolName == "ThunderBrowser" {
 		// 如果不是直接下载到目标路径，则进行转存
 		if t.TempDir != t.DstDirPath {
 			return transferObj(t.Ctx(), t.TempDir, t.DstDirPath, t.DeletePolicy)
 		}
+		return nil
+	}
+	if t.DeletePolicy == UploadDownloadStream {
+		dstStorage, dstDirActualPath, err := op.GetStorageAndActualPath(t.DstDirPath)
+		if err != nil {
+			return errors.WithMessage(err, "failed get dst storage")
+		}
+		taskCreator, _ := t.Ctx().Value("user").(*model.User)
+		task := &TransferTask{
+			TaskExtension: task.TaskExtension{
+				Creator: taskCreator,
+			},
+			SrcObjPath:   t.TempDir,
+			DstDirPath:   dstDirActualPath,
+			DstStorage:   dstStorage,
+			DstStorageMp: dstStorage.GetStorage().MountPath,
+			DeletePolicy: t.DeletePolicy,
+			Url:          t.Url,
+		}
+		task.SetTotalBytes(t.GetTotalBytes())
+		TransferTaskManager.Add(task)
 		return nil
 	}
 	return transferStd(t.Ctx(), t.TempDir, t.DstDirPath, t.DeletePolicy)
