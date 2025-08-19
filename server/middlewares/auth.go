073574a79c6bb7201d8d15a238f9277571f2eb26
MadDogOwner <xiaoran@xrgzs.top>
2025-08-19 15:23:10 +0800

    本次更改内容（8.19）6个内容(36)
    * 重构（docker）：减小docker镜像大小(2)
    * feat（share）：支持更安全的文件共享(28)
    * feat（server）：添加错误页面和状态码(2)重复server/handles/archive.go(1)重复server/middlewares/down.go(1)
    * feat（123_open）：添加选项 (4)
    * feat（服务器）：将 #1099 改编为 #991 (0)重复(3)[server/handles/archive.go=server/handles/down.go=server/handles/sharing.go]

--- server/middlewares/auth.go
+++ server/middlewares/auth.go
@@ -14,63 +14,65 @@ import (
 
 // Auth is a middleware that checks if the user is logged in.
 // if token is empty, set user to guest
-func Auth(c *gin.Context) {
-	token := c.GetHeader("Authorization")
-	if subtle.ConstantTimeCompare([]byte(token), []byte(setting.GetStr(conf.Token))) == 1 {
-		admin, err := op.GetAdmin()
+func Auth(allowDisabledGuest bool) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		token := c.GetHeader("Authorization")
+		if subtle.ConstantTimeCompare([]byte(token), []byte(setting.GetStr(conf.Token))) == 1 {
+			admin, err := op.GetAdmin()
+			if err != nil {
+				common.ErrorResp(c, err, 500)
+				c.Abort()
+				return
+			}
+			common.GinWithValue(c, conf.UserKey, admin)
+			log.Debugf("use admin token: %+v", admin)
+			c.Next()
+			return
+		}
+		if token == "" {
+			guest, err := op.GetGuest()
+			if err != nil {
+				common.ErrorResp(c, err, 500)
+				c.Abort()
+				return
+			}
+			if !allowDisabledGuest && guest.Disabled {
+				common.ErrorStrResp(c, "游客账户已禁用，请登录！", 401)
+				c.Abort()
+				return
+			}
+			common.GinWithValue(c, conf.UserKey, guest)
+			log.Debugf("use empty token: %+v", guest)
+			c.Next()
+			return
+		}
+		userClaims, err := common.ParseToken(token)
 		if err != nil {
-			common.ErrorResp(c, err, 500)
+			common.ErrorResp(c, err, 401)
 			c.Abort()
 			return
 		}
-		common.GinWithValue(c, conf.UserKey, admin)
-		log.Debugf("use admin token: %+v", admin)
-		c.Next()
-		return
-	}
-	if token == "" {
-		guest, err := op.GetGuest()
+		user, err := op.GetUserByName(userClaims.Username)
 		if err != nil {
-			common.ErrorResp(c, err, 500)
+			common.ErrorResp(c, err, 401)
 			c.Abort()
 			return
 		}
-		if guest.Disabled {
+		// validate password timestamp
+		if userClaims.PwdTS != user.PwdTS {
 			common.ErrorStrResp(c, "游客账户已禁用，请登录！", 401)
 			c.Abort()
 			return
 		}
-		common.GinWithValue(c, conf.UserKey, guest)
-		log.Debugf("use empty token: %+v", guest)
+		if user.Disabled {
+			common.ErrorStrResp(c, "当前账户已停用!", 401)
+			c.Abort()
+			return
+		}
+		common.GinWithValue(c, conf.UserKey, user)
+		log.Debugf("use login token: %+v", user)
 		c.Next()
-		return
 	}
-	userClaims, err := common.ParseToken(token)
-	if err != nil {
-		common.ErrorResp(c, err, 401)
-		c.Abort()
-		return
-	}
-	user, err := op.GetUserByName(userClaims.Username)
-	if err != nil {
-		common.ErrorResp(c, err, 401)
-		c.Abort()
-		return
-	}
-	// validate password timestamp
-	if userClaims.PwdTS != user.PwdTS {
-		common.ErrorStrResp(c, "密码已更改，请重新登录！", 401)
-		c.Abort()
-		return
-	}
-	if user.Disabled {
-		common.ErrorStrResp(c, "当前账户已停用", 401)
-		c.Abort()
-		return
-	}
-	common.GinWithValue(c, conf.UserKey, user)
-	log.Debugf("use login token: %+v", user)
-	c.Next()
 }
 
 func Authn(c *gin.Context) {
@@ -130,7 +132,7 @@ func Authn(c *gin.Context) {
 func AuthNotGuest(c *gin.Context) {
 	user := c.Request.Context().Value(conf.UserKey).(*model.User)
 	if user.IsGuest() {
-		common.ErrorStrResp(c, "You are a guest", 403)
+		common.ErrorStrResp(c, "您是访客!", 403)
 		c.Abort()
 	} else {
 		c.Next()
